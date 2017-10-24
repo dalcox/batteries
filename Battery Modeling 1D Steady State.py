9f6bf452e8508a40685a7c137d2c6a9de0e3c537
Dalton Cox <daltoncox@Daltons-MacBook-Air.local>
2017-10-24 18:20:33 -0400

    Tafel Kinetics now working
    Full Butler-Volmer kinetics the next step

--- Battery Modeling 1D Steady State.py
+++ Battery Modeling 1D Steady State.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[1]:
+# In[12]:
 
 import scipy as sp
 import numpy as np
@@ -11,7 +11,7 @@ from scipy.optimize import fsolve
 get_ipython().magic('matplotlib inline')
 
 
-# In[2]:
+# In[13]:
 
 # initializing constants
 K = 0.06    #liquid conductivity
@@ -29,18 +29,18 @@ I = 0.1     #total current density
 
 
 #graph analytically
-X = np.linspace(0., L, 100)
+X = np.linspace(0., L, 200)
 Y=y = X/L
 v = L*np.sqrt(a*io*(n*F)/(R*T)*(K + s)/(K*s))
 i2 = I * K/(K + s)*(1 + (s*(K**-1)*np.sinh(v*(1-y)) - np.sinh(v*y))/np.sinh(v))
 
 
-# In[3]:
+# In[14]:
 
 plt.plot(X, i2)
 
 
-# In[4]:
+# In[50]:
 
 #solve numerically
 
@@ -63,19 +63,32 @@ def linearbattfunc(IV, x):
 def BVbattfunc(IV, x):
     i1, i2, V1, V2 = IV
     di2 = -a*io*np.exp((n*F)/(R*T)*-ac*(V1 - V2))
+#     di2 = a*io*(n*F)/(R*T)*(V1 - V2)
     di1 = -di2
     dV1 = -i1/s
     dV2 = -i2/K
     return di1, di2, dV1, dV2
 
+def Tafelfunc(IV, x):
+    """
+    uses Tafel kinetics
+    """
+    i1, i2, V1, V2 = IV
+    
+    di2 = -a*io*np.exp(-ac*n*F/(R*T)*(V1 - V2))
+    di1 = -di2
+    dV1 = -i1/s
+    dV2 = -i2/K
+    
+    return di1, di2, dV1, dV2
 
-# In[5]:
+
+# In[78]:
 
 def objective(u2_0):
     """
     The thing we want to set equal to zero
     """
-    
     U = odeint(simplebattfunc, [I, u2_0], X)
 #     print(U[-1,0])
     return U[-1,0]
@@ -85,9 +98,9 @@ def objectiveLinear(V2o):
     """
     Shooting method for solving linear kinetic system
     """
-    
-    V = odeint(linearbattfunc, [0, I, 0, V2o], X)
+    V = odeint(linearbattfunc, [0., I, 0., V2o], X)
     dV = np.diff(V[:,3])/np.diff(X)
+#     print(dV[-1])
     return dV[-1]
 
 
@@ -95,37 +108,85 @@ def objectiveBV(V2o):
     """
     Shooting method for solving Butler-Volmer kinetics system (Tafel)
     """
-    
-    V = odeint(BVbattfunc, [0, I, 0, V2o], X)
+    V = odeint(BVbattfunc, [0., I, 0., V2o], X)
     dV = np.diff(V[:,3])/np.diff(X)
 #     print(dV[-1])
     return dV[-1]
 
+def ObjectiveTafel(IVo):
+    """
+    Solves initial condition for tafel kinetic problem
+    """
+    i1o, V2o = IVo
+    U = odeint(Tafelfunc, [i1o, I, 0, V2o], X)
+    V = U[:,3]
+    dV = np.diff(V)/np.diff(X)
+    
+    return U[0,0], dV[-1]
 
-# In[10]:
 
-u2_0, = fsolve(objective, 0)
-simple_i = odeint(simplebattfunc, [I, u2_0], X)
-simple_i1 = I - simple_i[:,0]
+# In[79]:
 
-linear_V2_0 = fsolve(objectiveLinear, 0)
-linear_IV = odeint(linearbattfunc, [0, I, 0, linear_V2_0], X)
+# u2_0, = fsolve(objective, 0.)
+# simple_i = odeint(simplebattfunc, [I, u2_0], X)
+# simple_i1 = I - simple_i[:,0]
 
-# BV_V2_0, = fsolve(objectiveBV, 0)
-# BV_IV = odeint(BVbattfunc, [0, I, 0, BV_V2_0], X)
+linear_V2_0 = fsolve(objectiveLinear, 0.)
+linear_IV = odeint(linearbattfunc, [0., I, 0., linear_V2_0], X)
 
-plt.plot(X, simple_i[:,0], label = 'ionic linear simple')
-plt.plot(X, simple_i1, label = 'electronic linear simple')
-plt.plot(X, linear_IV[:,0], label = 'ionic linear')
-plt.plot(X, linear_IV[:,1], label = 'electronic linear')
-# plt.plot(X, BV_IV[:,0], label = 'ionic Tafel')
-# plt.plot(X, BV_IV[:,1], label = 'electronic Tafel')
+# BV_V2_0 = fsolve(objectiveBV, 0)
+# BV_IV = odeint(BVbattfunc, [0., I, 0, BV_V2_0], X)
+
+T_i1o, T_V2o = fsolve(ObjectiveTafel, [0,0])
+T_IV = odeint(Tafelfunc, [0, I, 0, V2o], X)
+
+# plt.plot(X, simple_i[:,0], label = 'ionic linear simple')
+# plt.plot(X, simple_i1, label = 'electronic linear simple')
+plt.plot(X, linear_IV[:,0], '--', color = 'r', label = 'electronic linear')
+plt.plot(X, linear_IV[:,1], '--',color = 'b' ,label = 'ionic linear')
+plt.plot(X, T_IV[:,0], color = 'r', label = 'electronic Tafel')
+plt.plot(X, T_IV[:,1], color = 'b', label = 'ionic Tafel')
 plt.legend(loc = 'best')
 
 
-# In[7]:
+# In[72]:
 
-get_ipython().magic('pinfo plt.plot')
+def Tafelfunc(IV, x):
+    """
+    uses Tafel kinetics
+    """
+    i1, i2, V1, V2 = IV
+    
+    di2 = -a*io*np.exp(-ac*n*F/(R*T)*(V1 - V2))
+    di1 = -di2
+    dV1 = -i1/s
+    dV2 = -i2/K
+    
+    return di1, di2, dV1, dV2
+
+def ObjectiveTafel(IVo):
+    """
+    Solves initial condition for tafel kinetic problem
+    """
+    i1o, V2o = IVo
+    U = odeint(Tafelfunc, [i1o, I, 0, V2o], X)
+    V = U[:,3]
+    dV = np.diff(V)/np.diff(X)
+    
+    return U[0,0], dV[-1]
+
+
+# In[68]:
+
+i1o, V2o = fsolve(ObjectiveTafel, [0,0])
+U = odeint(Tafelfunc, [0, I, 0, V2o], X)
+
+plt.plot(X, U)
+
+
+# In[69]:
+
+get_ipython().magic('pinfo odeint')
 
 
 # In[ ]:
