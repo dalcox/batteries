f52dec8d43a2ebd2566d3e9e237d7470c06f5911
Dalton Cox <daltoncox@Daltons-MacBook-Air.local>
2017-11-20 17:59:29 -0500

    Fully functional boundary value solver
    solve_bvp function now integrated into solution - very fast solving

--- Battery Modeling 1D Steady State.py
+++ Battery Modeling 1D Steady State.py
@@ -11,7 +11,7 @@ from scipy.optimize import fsolve
 get_ipython().magic('matplotlib inline')
 
 
-# In[103]:
+# In[2]:
 
 # initializing constants
 K = 0.06    #liquid conductivity
@@ -35,12 +35,12 @@ v = L*np.sqrt(a*io*(n*F)/(R*T)*(K + s)/(K*s))
 i2 = I * K/(K + s)*(1 + (s*(K**-1)*np.sinh(v*(1-y)) - np.sinh(v*y))/np.sinh(v))
 
 
-# In[104]:
+# In[3]:
 
 plt.plot(X, i2)
 
 
-# In[105]:
+# In[4]:
 
 #solve numerically
 
@@ -52,7 +52,7 @@ def simplebattfunc(i, x):
     return di, d2i
 
 #systems of differential equations with drop-in replacements possible
-def linearbattfunc(IV, x):
+def linearbattfunc(x, IV):
     i1, i2, V1, V2 = IV
     di2 = a*io*(n*F)/(R*T)*(V1 - V2)
     di1 = -di2
@@ -60,7 +60,9 @@ def linearbattfunc(IV, x):
     dV2 = -i2/K
     return di1, di2, dV1, dV2
 
-def BVbattfunc(IV, x):
+
+
+def BVbattfunc(x, IV):
     i1, i2, V1, V2 = IV
     di2 = a*io*(np.exp((n*F)/(R*T)*aa*(V1 - V2)) - np.exp((n*F)/(R*T)*-ac*(V1 - V2)))
 #     di2 = a*io*(n*F)/(R*T)*(V1 - V2)
@@ -75,7 +77,11 @@ def Tafelfunc(IV, x):
     """
     i1, i2, V1, V2 = IV
     
-    di2 = -a*io*np.exp(-ac*n*F/(R*T)*(V1 - V2))
+    taff = -ac*n*F/(R*T)*(V1-V2)
+    
+    #di2 = -a*io*np.exp(-ac*n*F/(R*T)*(V1 - V2))
+    di2 = -a*io*np.exp(taff)
+    #di2 = -a*io*(1 + taff + (taff**2)/2 + (taff**3)/6 + (taff**4)/24 + (taff**5)/120 + (taff**6)/720)
     di1 = -di2
     dV1 = -i1/s
     dV2 = -i2/K
@@ -83,76 +89,74 @@ def Tafelfunc(IV, x):
     return di1, di2, dV1, dV2
 
 
-# In[106]:
+# In[5]:
 
-def objective(u2_0):
-    """
-    The thing we want to set equal to zero
-    """
-    U = odeint(simplebattfunc, [I, u2_0], X)
-#     print(U[-1,0])
-    return U[-1,0]
+# def objective(u2_0):
+#     """
+#     The thing we want to set equal to zero
+#     """
+#     U = odeint(simplebattfunc, [I, u2_0], X)
+# #     print(U[-1,0])
+#     return U[-1,0]
 
-def objectiveLinear(V2o):
-    """
-    Shooting method for solving linear kinetic system
-    """
-    V = odeint(linearbattfunc, [0., I, 0., V2o], X)
-    dV = np.diff(V[:,3])/np.diff(X)
-#     print(dV[-1])
-    return dV[-1]
+# def objectiveLinear(V2o):
+#     """
+#     Shooting method for solving linear kinetic system
+#     """
+#     V = odeint(linearbattfunc, [0., I, 0., V2o], X)
+#     dV = np.diff(V[:,3])/np.diff(X)
+# #     print(dV[-1])
+#     return dV[-1]
 
-def objectiveBV(V2o):
-    """
-    Shooting method for solving Butler-Volmer kinetics system (Tafel)
-    """
-    V = odeint(BVbattfunc, [0., I, 0., V2o], X)
-    dV = np.diff(V[:,3])/np.diff(X)
-#     print(dV[-1])
-    return dV[-1]
+# def objectiveBV(V2o):
+#     """
+#     Shooting method for solving Butler-Volmer kinetics system (Tafel)
+#     """
+#     V = odeint(BVbattfunc, [0., I, 0., V2o], X)
+#     dV = np.diff(V[:,3])/np.diff(X)
+# #     print(dV[-1])
+#     return dV[-1]
 
-def ObjectiveTafel(IVo):
-    """
-    Solves initial condition for tafel kinetic problem
-    """
-    i1o, V2o = IVo
-    U = odeint(Tafelfunc, [i1o, I, 0, V2o], X)
-    V = U[:,3]
-    dV = np.diff(V)/np.diff(X)
-    print(i1o, V2o)
+# def ObjectiveTafel(IVo):
+#     """
+#     Solves initial condition for tafel kinetic problem
+#     """
+#     i1o, V2o = IVo
+#     U = odeint(Tafelfunc, [i1o, I, 0, V2o], X)
+#     V = U[:,3]
+#     dV = np.diff(V)/np.diff(X)
+#     print(i1o, V2o)
     
-    return U[0,0], dV[-1]
+#     return U[0,0], dV[-1]
 
 
-# In[131]:
+# In[6]:
 
 # u2_0, = fsolve(objective, 0.)
 # simple_i = odeint(simplebattfunc, [I, u2_0], X)
-# simple_i1 = I - simple_i[:,0]
+# # simple_i1 = I - simple_i[:,0]
 
-#linear_V2_0 = fsolve(objectiveLinear, 0.)
-linear_IV = odeint(linearbattfunc, [0., I, 0., linear_V2_0], X) #, full_output = 1)
+# # linear_V2_0 = fsolve(objectiveLinear, 0.)
+# # linear_IV = odeint(linearbattfunc, [0., I, 0., linear_V2_0], X) #, full_output = 1)
 
-# BV_i1_0, BV_V2_0 = fsolve(bjectiveTafel, [0,0])
-# BV_IV = odeint(BVbattfunc, [0., I, 0, BV_V2_0], X)
-T_i1o, T_V2o = fsolve(ObjectiveTafel, [0.1, .3], maxfev = 50)
-T_IV = odeint(Tafelfunc, [T_i1o, I, 0, T_V2o], X) #, full_output = 1)
-# print(T_IV)
+# # BV_i1_0, BV_V2_0 = fsolve(bjectiveTafel, [0,0])
+# # BV_IV = odeint(BVbattfunc, [0., I, 0, BV_V2_0], X)
+# T_i1o, T_V2o = fsolve(ObjectiveTafel, [0.1, 0.299], maxfev = 50)
+# T_IV = odeint(Tafelfunc, [T_i1o, I, 0, T_V2o], X) #, full_output = 1)
+# # print(T_IV)
 
-# plt.plot(X, simple_i[:,0], label = 'ionic linear simple')
-# plt.plot(X, simple_i1, label = 'electronic linear simple')
+# # plt.plot(X, simple_i[:,0], label = 'ionic linear simple')
+# # plt.plot(X, simple_i1, label = 'electronic linear simple')
 
-plt.figure()
-plt.plot(X, linear_IV[:,0], '--', color = 'r', label = 'electronic linear')
-plt.plot(X, linear_IV[:,1], '--',color = 'b' ,label = 'ionic linear')
-plt.plot(X, T_IV[:,0], color = 'r', label = 'electronic Tafel')
-plt.plot(X, T_IV[:,1], color = 'b', label = 'ionic Tafel')
-plt.legend(loc = 'best')
+# plt.figure()
+# # plt.plot(X, linear_IV[:,0], '--', color = 'r', label = 'electronic linear')
+# # plt.plot(X, linear_IV[:,1], '--',color = 'b' ,label = 'ionic linear')
+# plt.plot(X, T_IV[:,0], color = 'r', label = 'electronic Tafel')
+# plt.plot(X, T_IV[:,1], color = 'b', label = 'ionic Tafel')
+# plt.plot(X, T_IV[:,2], color = 'r', label = 'electronic Tafel')
+# plt.plot(X, T_IV[:,3], color = 'b', label = 'ionic Tafel')
 
-
-# In[ ]:
-
-
+# plt.legend(loc = 'best')
 
 
 # In[ ]:
@@ -160,7 +164,12 @@ plt.legend(loc = 'best')
 
 
 
-# In[26]:
+# In[ ]:
+
+
+
+
+# In[7]:
 
 def Tafelfunc(IV, x):
     """
@@ -189,8 +198,54 @@ def ObjectiveTafel(IVo):
 
 # In[8]:
 
-i1o, V2o = fsolve(ObjectiveTafel, [0,0])
-U = odeint(Tafelfunc, [i1o, I, 0, V2o], X)
+from scipy.integrate import solve_bvp
 
-plt.plot(X, U)
+def BC(ya, yb):
+    return (yb[0] - I), (ya[1] - I), ya[2], yb[1]
+
+def linearbattfunc(x, IV):
+    i1, i2, V1, V2 = IV
+    di2 = a*io*(n*F)/(R*T)*(V1 - V2)
+    di1 = -di2
+    dV1 = -i1/s
+    dV2 = -i2/K
+    return di1, di2, dV1, dV2
+
+def Tafelfunc(x, IV):
+    """
+    uses Tafel kinetics
+    """
+    i1, i2, V1, V2 = IV
+    
+    taff = -ac*n*F/(R*T)*(V1-V2)
+    
+    #di2 = -a*io*np.exp(-ac*n*F/(R*T)*(V1 - V2))
+    di2 = -a*io*np.exp(taff)
+    #di2 = -a*io*(1 + taff + (taff**2)/2 + (taff**3)/6 + (taff**4)/24 + (taff**5)/120 + (taff**6)/720)
+    di1 = -di2
+    dV1 = -i1/s
+    dV2 = -i2/K
+    
+    return di1, di2, dV1, dV2
+
+
+# In[9]:
+
+x = np.linspace(0, L, 10)
+y = np.zeros([4,10])
+y[:,0] = [0., I, .1, .1]
+y[:,-1] = [I, 0., .1, .1]
+
+linear_IV = solve_bvp(BVbattfunc, BC, x, y)
+
+x_plot = np.linspace(0, L, 100)
+
+for i in range(4):
+#     plt.plot(x, y[i])
+    plt.plot(x_plot, linear_IV.sol(x_plot)[i])
+
+
+# In[ ]:
+
+
 
