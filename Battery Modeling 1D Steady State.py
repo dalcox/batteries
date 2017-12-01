0f265e25ace33b7584a44dbf63d7343637d34dca
Dalton Cox <daltoncox@Daltons-MacBook-Air.local>
2017-11-30 20:23:47 -0500

    Functional anode, cathode, and separator
    First draft of full battery configuration achieved

--- Battery Modeling 1D Steady State.py
+++ Battery Modeling 1D Steady State.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[1]:
+# In[3]:
 
 import scipy as sp
 import numpy as np
@@ -9,10 +9,12 @@ import BATT as b
 import matplotlib.pyplot as plt
 from scipy.integrate import odeint
 from scipy.optimize import fsolve
+from scipy.integrate import solve_bvp
+
 get_ipython().magic('matplotlib inline')
 
 
-# In[2]:
+# In[62]:
 
 # initializing constants
 K = 0.06    #liquid conductivity
@@ -27,6 +29,8 @@ F = 96485   #Faraday's constant
 R = 8.314   #gas constant
 T = 298     #temperature
 I = 0.1     #total current density
+L_sep = 0.1 #length of separator
+Resist_sep = 1 #resistance of separator per length
 
 
 # graph analytically
@@ -147,7 +151,6 @@ def Tafelfunc_c(x, IV):
 
 # In[5]:
 
-from scipy.integrate import solve_bvp
 
 def BC_c(ya, yb):
     return (yb[0] - I), (ya[1] - I), ya[3], yb[1]
@@ -158,30 +161,30 @@ def BC_a(ya, yb):
 
 # In[6]:
 
-x = np.linspace(0, L, 10)
-y = np.ones([4, 10])
+# x = np.linspace(0, L, 10)
+# y = np.ones([4, 10])
 
-# y[:,0] = [0., I, .1, .1]
-# y[:,-1] = [I, 0., .1, .1]
+# # y[:,0] = [0., I, .1, .1]
+# # y[:,-1] = [I, 0., .1, .1]
 
-linear_IV = solve_bvp(linearbattfunc, BC_a, x, y)
-Ta_IV = solve_bvp(Tafelfunc_a, BC_a, x, y)
-Tc_IV = solve_bvp(Tafelfunc_c, BC_c, x, y)
+# linear_IV = solve_bvp(linearbattfunc, BC_a, x, y)
+# Ta_IV = solve_bvp(Tafelfunc_a, BC_a, x, y)
+# Tc_IV = solve_bvp(Tafelfunc_c, BC_c, x, y)
 
-x_plot = np.linspace(0, L, 100)
+# x_plot = np.linspace(0, L, 100)
 
-plt.figure(0)
-for i in range(2):
-#     plt.plot(x, y[i])
-#     plt.plot(x_plot, linear_IV.sol(x_plot)[i])
-    plt.plot(x_plot, Ta_IV.sol(x_plot)[i], color = 'r')
-    plt.plot(x_plot, Tc_IV.sol(x_plot)[i], color = 'b')
+# plt.figure(0)
+# for i in range(2):
+# #     plt.plot(x, y[i])
+# #     plt.plot(x_plot, linear_IV.sol(x_plot)[i])
+#     plt.plot(x_plot, Ta_IV.sol(x_plot)[i], color = 'r')
+#     plt.plot(x_plot, Tc_IV.sol(x_plot)[i], color = 'b')
     
-plt.figure(1)
-for i in range(2, 4):
-#     plt.plot(x_plot, linear_IV.sol(x_plot)[i])
-    plt.plot(x_plot, Ta_IV.sol(x_plot)[i], color = 'r')
-    plt.plot(x_plot, Tc_IV.sol(x_plot)[i], color = 'b')
+# plt.figure(1)
+# for i in range(2, 4):
+# #     plt.plot(x_plot, linear_IV.sol(x_plot)[i])
+#     plt.plot(x_plot, Ta_IV.sol(x_plot)[i], color = 'r')
+#     plt.plot(x_plot, Tc_IV.sol(x_plot)[i], color = 'b')
 
 
 # In[7]:
@@ -220,7 +223,8 @@ def TafelOverall(x, IV):
 
 #     return di1, di2, dV1, dV2
     is_p, ie_p, Vs_p, Ve_p, is_n, ie_n, Vs_n, Ve_n = IV
-
+    #n represents anode attributes, p represents cathode attributes
+    
     taff_p = -ac*n*F/(R*T)*(Vs_p - Ve_p)
     taff_n = aa*n*F/(R*T)*(Vs_n - Ve_n)
     
@@ -273,14 +277,114 @@ c = np.append(Mashed.sol(x_plot)[6], Mashed.sol(x_plot)[2])
 d = np.append(Mashed.sol(x_plot)[7], Mashed.sol(x_plot)[3])
 
 
-# In[13]:
+# In[11]:
 
 x_plot2 = np.linspace(0, 2*L, 200)
 data = [a,b,c,d]
 labels = ['solid current','electrolyte current','solid voltage','electrolyte voltage']
 for i in range(4):
-    plt.plot(x_plot2, data[i], label = labels[i])
+    plt.plot(x_plot2, data[i], label = labels[i], marker = '.', linestyle = 'none')
     plt.legend(loc = 'best')
+
+
+# In[57]:
+
+Vwall1 = 1
+Vwall2 = 1
+
+def BCCath(ya, yb):
+    return (yb[0] - I), (ya[1] - I), ya[3] - Vwall_2, yb[1]
+#     return (ya[0] - I), yb[0], (yb[1] - I), ya[2]# (yb[4] - I), ya[4], (ya[5] - I), ya[2], (ya[6])
+
+def BCAn(ya, yb):
+    return (yb[1] - I), (ya[0] - I), yb[3], yb[0]
+#     return (ya[0] - I), yb[0], (yb[1] - I), ya[2]# (yb[4] - I), ya[4], (ya[5] - I), ya[2], (ya[6])
+      
+def TafelCath(x, IV):
+    """
+    uses Tafel kinetics
+    """
+    is_p, ie_p, Vs_p, Ve_p = IV
+    #n represents anode attributes, p represents cathode attributes
+    
+    taff_p = -ac*n*F/(R*T)*(Vs_p - Ve_p)
+    die_p = -a*io*np.exp(taff_p)
+    #Kinetics
+    
+    dis_p = -die_p
+    #charge neutrality
+    
+    dVs_p = -is_p/s
+    #solids ohms law
+    
+    dVe_p = -ie_p/K
+    #liquids ohms law
+
+    return dis_p, die_p, dVs_p, dVe_p
+
+def TafelAn(x, IV):
+    """
+    uses Tafel kinetics
+    """
+    
+    is_n, ie_n, Vs_n, Ve_n = IV
+    #n represents anode attributes, p represents cathode attributes
+    
+    taff_n = aa*n*F/(R*T)*(Vs_n - Ve_n)
+    die_n = a*io*np.exp(taff_n)
+    #Kinetics
+    
+    dis_n = -die_n
+    #charge neutrality
+    
+    dVs_n = -is_n/s
+    #solids ohms law
+    
+    dVe_n = -ie_n/K
+    #liquids ohms law
+
+    return dis_n, die_n, dVs_n, dVe_n
+
+
+# In[63]:
+
+N = 10
+N_sep = 20
+x_cath = np.linspace(L + L_sep, 2*L + L_sep, N)
+x_an = np.linspace(0, L, N)
+y = np.zeros([4, N])
+
+x_plot_cath = np.linspace(L + L_sep, L_sep + 2*L, 100)
+x_plot_an = np.linspace(0, L, 100)
+
+An = solve_bvp(TafelAn, BCAn, x_an, y)
+Vwall_1 = An.sol(x_plot)[3][-1]
+
+x_sep = np.linspace(L, L + L_sep, N_sep)
+is_sep = np.ones(N_sep) * An.sol(x_plot_an)[0][-1]
+ie_sep = np.ones(N_sep) * An.sol(x_plot_an)[1][-1]
+Ve_sep = np.zeros(N_sep)
+Ve_sep[0] = Vwall_1
+for i in range(N_sep):
+    Ve_sep[i] = Vwall_1 - (x_sep[i] - L) * Resist_sep
+
+Vwall_2 = Ve_sep[-1]
+Cath = solve_bvp(TafelCath, BCCath, x_cath, y)
+# Ta_IV = solve_bvp(Tafelfunc_a, BC_a, x, y)
+# Tc_IV = solve_bvp(Tafelfunc_c, BC_c, x, y)
+
+for i in range(4):
+    plt.plot(x_plot_cath, Cath.sol(x_plot_cath)[i], color = 'r')
+    plt.plot(x_plot_an, An.sol(x_plot_an)[i], color = 'b')
+
+plt.plot(x_sep, Ve_sep, color = 'g')
+plt.plot(x_sep, is_sep, color = 'g')
+plt.plot(x_sep, ie_sep, color = 'g')
+
+
+# In[39]:
+
+print(Cath.sol(x_plot)[3][-1])
 
 
 # In[ ]:
